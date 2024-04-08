a7f3a11db20e65294da8da35cea088e035957ab7
MariusPopa1 <2003popamarius@gmail.com>
2024-04-08 15:12:19 +0300

    im the best(+Max)

--- lab4/Operations.py
+++ lab4/Operations.py
@@ -1,4 +1,4 @@
-import random
+import random as r
 
 
 def split(expression):
@@ -9,20 +9,21 @@ def split(expression):
         if expression[count] == '(':
             bracket_count += 1
             end_index = expression.find(')', count)
-            elements.append("|" + expression[count+1:end_index] + "|")
-            count = end_index
+            elements.append(expression[count+1:end_index] + "|")
+            count = end_index+1
         elif expression[count] in "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890":
             elements.append(expression[count])
             count += 1
         elif expression[count] in "+*?":
             elements[-1] = elements[-1] + expression[count]
             count += 1
+        elif expression[count] == '^':
+            elements[-1] = elements[-1] + expression[count:count + 2]
+            count += 2
         else:
             count += 1
-        if expression[count] == '^':
-            elements[-1] = elements[-1] + expression[count:count+2]
-            count += 2
-    print(elements)
+    print("Separated elements of the expression: ", elements)
+
     return elements
 
 
@@ -36,15 +37,17 @@ def assign(elements):
             codes_list[count] = 1  # Always Show
             continue
         if elements[count].count('|') != 0:
-            codes_list[count] = 20  # Choose one
             if elements[count].count('*') != 0:
                 codes_list[count] = 21  # 0 up to 5
-            if elements[count].count('?') != 0:
+            elif elements[count].count('?') != 0:
                 codes_list[count] = 22  # Maybe show
-            if elements[count].count('+') != 0:
+            elif elements[count].count('+') != 0:
                 codes_list[count] = 23  # 1 up to 5
-            if elements[count].count('^') != 0:
+            elif elements[count].count('^') != 0:
                 codes_list[count] = 24
+            else:
+                codes_list[count] = 20  # Choose one
+
         else:
             if elements[count].count('*') != 0:
                 codes_list[count] = 3  # 0 up to 5
@@ -54,13 +57,62 @@ def assign(elements):
                 codes_list[count] = 5  # 1 up to 5
             if elements[count].count('^') != 0:
                 codes_list[count] = 6
+
+    print("Coded list for each of the elements: ", codes_list)
+
     return codes_list, elements
 
 
 def output(codes_list, separated):
     final_string = ""
+
     for count in range(len(codes_list)):
         if codes_list[count] == 1:
             final_string += separated[count]
+        else:
+            if codes_list[count] // 10 == 2:
+                temp_string = separated[count]
+                bars = temp_string.count('|')
+                choice = r.randint(0, bars-1)
+                winner = ""
+                mark1 = -1
+                bar_count = 0
+
+                for symbol_num in range(len(temp_string)):
+                    if bar_count == choice and mark1 == -1:
+                        mark1 = symbol_num
+                    if temp_string[symbol_num] == '|':
+                        bar_count += 1
+                    if bar_count == choice+1:
+                        mark2 = symbol_num
+                        winner = temp_string[mark1:mark2]
+                        bar_count += 1
+
+                if codes_list[count] == 20:
+                    final_string += winner
+                elif codes_list[count] == 21:
+                    nr_show = r.randint(0, 5)
+                    final_string += winner * nr_show
+                elif codes_list[count] == 22:
+                    nr_show = r.randint(0, 1)
+                    final_string += winner * nr_show
+                elif codes_list[count] == 23:
+                    nr_show = r.randint(1, 5)
+                    final_string += winner * nr_show
+                elif codes_list[count] == 24:
+                    final_string += winner * int(separated[count][-1])
+            elif codes_list[count] == 3:
+                nr_show = r.randint(0, 5)
+                final_string += separated[count][0] * nr_show
+            elif codes_list[count] == 4:
+                nr_show = r.randint(0, 1)
+                final_string += separated[count][0] * nr_show
+            elif codes_list[count] == 5:
+                nr_show = r.randint(1, 5)
+                final_string += separated[count][0] * nr_show
+            elif codes_list[count] == 6:
+                final_string += separated[count][0] * int(separated[count][-1])
+
+    print("The final randomly generated output: ", final_string)
 
     return final_string
--- lab4/main.py
+++ lab4/main.py
@@ -1,9 +1,11 @@
 from Operations import *
 
 
-expression = "O(P|Q|R)+2(3|4)^2Q+Q*Q^2"
-separated = split(expression)
-codes_list, separated = assign(separated)
-print(codes_list)
-print(separated)
-print(output(codes_list, separated))
+expression_list = ["(S|T)(U|V)W*Y+24", "L(M|N)O^3P*Q(2|3)", "R*S(T|U|V)W(X|Y|Z)^2", "(a|b)(c|d)E*G?",
+                   "P(Q|R|S)T(UV|W|X)*Z+", "1(0|1)*2(3|4)^536"]
+for expression in expression_list:
+    print("Given Expression: ", expression)
+    separated = split(expression)
+    codes_list, separated = assign(separated)
+    final_result = output(codes_list, separated)
+    print("\n")
